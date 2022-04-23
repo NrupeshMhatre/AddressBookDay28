003d6c24bf4fe15657eeea62fd6ac50cbb596241
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-04-23 14:36:23 +0530

    Read and Write the Address Book csv file

---
+++ src/test/java/practice/AppTest.java
@@ -0,0 +1,15 @@
+package practice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+	
+	}
+
+}
